ce4e83f4cbf71c338a6b18fb6c87e5aa8634ef30
asha-it22027 <it22027@mbstu.ac.bd>
2025-07-22 13:12:33 +0600

    Add files via upload

---
+++ SpringPro/src/test/java/com/Springpro/SpringPro/SpringProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springpro.SpringPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
